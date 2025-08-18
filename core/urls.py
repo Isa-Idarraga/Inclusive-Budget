7009ac54269ca73dcdb0655db690027ace249d48
Isa idarraga <iidarragab@eafit.edu.co>
2025-08-17 23:06:59 -0500

    chore(core): bootstrap de proyecto (manage, urls, asgi, wsgi)

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+# core/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("dashboard.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),  # <- esto
+]
+
